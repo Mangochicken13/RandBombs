42343bcb735da2d1ce91b16f8de3daa798d8b60f
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2023-08-06 22:03:49 +0800

    Update Mine-Sweeper Item
    slightly tweaked the behaviour of the "Mine Sweeper", and added it to the demolitionist's shop for 10 gold

---
+++ BombGNPC.cs
@@ -0,0 +1,17 @@
+﻿using RandBombs.Items;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RandBombs
+{
+    public class BombGNPC : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            if (shop.NpcType == NPCID.Demolitionist)
+            {
+                shop.Add<MineSweeper>();
+            }
+        }
+    }
+}
